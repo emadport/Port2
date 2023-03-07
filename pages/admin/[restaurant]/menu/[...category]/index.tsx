37e936f5d95e5249672a297776d05885c5027cde
emadi65 <emad.askari@gmail.com>
2023-03-07 19:18:42 +0100

    Find a solution for ui delay

--- pages/admin/[restaurant]/menu/[...category]/index.tsx
+++ pages/admin/[restaurant]/menu/[...category]/index.tsx
@@ -68,16 +68,12 @@ export default function Category() {
 
   function onSelectionChange(e: SelectChangeEvent) {
     setActionType(e.target.value);
-
-    startTransition(() => {
-      setImportedItems([...allItems?.FetchAllMenuItems]);
-    });
   }
-  // useEffect(() => {
-  //   if (allItems?.FetchAllMenuItems?.length) {
-  //     setImportedItems([...allItems?.FetchAllMenuItems]);
-  //   }
-  // }, [actionType, allItems?.FetchAllMenuItems]);
+  useEffect(() => {
+    if (allItems?.FetchAllMenuItems?.length && actionType === "import item") {
+      setImportedItems([...allItems?.FetchAllMenuItems]);
+    }
+  }, [actionType, allItems?.FetchAllMenuItems]);
 
   const MenuItems = importedItems.map((item) => {
     return (
@@ -195,9 +191,7 @@ export default function Category() {
                 category={query.category?.[0] as string}
                 subCat={query?.category}></MenuAdder>
             )}
-          {actionType === "import item" && importedItems.length && (
-            <div>{MenuItems}</div>
-          )}
+          {importedItems?.length && <div>{MenuItems}</div>}
           {errorOnSavingItem && (
             <ErrorCard>There was an error during creation</ErrorCard>
           )}
