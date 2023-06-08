9dcb81ea3c2a9252937f35904b727b1803ce3a75
emadi65 <emad.askari@gmail.com>
2023-06-08 22:53:47 +0200

    Change about description

--- README.md
+++ README.md
@@ -11,6 +11,5 @@ yarn dev
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 Welcome to our React-based food ordering app! The app uses Nextjs, Scss,
 Typescript, Graphql and websockets and it is only for testing
-purposes. The app designed to show all ability in one app, thats because it uses different techniques. The app is 70% done and there is lots of error handling still
-left in this project to be done. is right now just for testing purposes
-and is not in commercial level.
+purposes and show some skills. The portfolio designed to show different ability and skills all in one place. The app is 70% done and there is lots of error handling and performance optimzation still
+left to be done.
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -9,11 +9,10 @@ export default function about({}: Props) {
       <h1 className={styles.about__header}>About Beställät App</h1>
       <div className={styles.about__paragraph}>
         Welcome to our React-based food ordering app! The app uses Nextjs, Scss,
-        Typescript, Graphql and websockets and it`s only for testing
-        purposes.The app designed to show all ability in one app, thats because
-        it uses different techniques. The app is 70% done and there is lots of
-        error handling still left in this project to be done. is right now just
-        for testing purposes and is not in commercial level.
+        Typescript, Graphql and websockets and it is only for testing purposes
+        and show some skills. The portfolio designed to show different ability
+        and skills all in one place. The app is 70% done and there is lots of
+        error handling and performance optimzation still left to be done.
       </div>
     </div>
   );
