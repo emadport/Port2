7d01472c46e96d56ce45598c9b83cbe75e256b7c
Emad Askari <emad.askari@gmail.com>
2023-06-09 17:28:40 +0200

    Update [restaurantName] with performence

--- pages/api/reservation/[restaurantName].ts
+++ pages/api/reservation/[restaurantName].ts
@@ -1,5 +1,4 @@
-import { NextApiResponse } from "next";
-import { NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import Reservation from "@/server/mongoSchema/reservationSchema";
 import { Types } from "mongoose";
 
@@ -12,38 +11,34 @@ export const config = {
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const costumerId = req.cookies["costumerId"];
   const id = new Types.ObjectId(costumerId);
-  const restaurantName = req.query.restaurantName;
-  if (req.method === "POST") {
-    // const { date } = req.body;
+  const restaurantName = req.query.restaurantName as string;
 
-    const reservation = await new Reservation({
-      restaurant: restaurantName,
-      costumer: id,
-      date: new Date(),
-      desription: "okkokok",
-    });
-    await reservation.save();
-    res.status(200).send(reservation);
-  } else if (req.method === "GET") {
-    const date = [];
-    // Handle any other HTTP method
+  try {
+    if (req.method === "POST") {
+      const reservation = new Reservation({
+        restaurant: restaurantName,
+        costumer: id,
+        date: new Date(),
+        description: "okkokok",
+      });
+      await reservation.save();
+      res.status(200).send(reservation);
+    } else if (req.method === "GET") {
+      const oldReservations = await Reservation.find({
+        restaurant: restaurantName,
+      }).populate("costumer");
 
-    const oldReservation = await Reservation.find({
-      restaurant: restaurantName,
-    }).populate("costumer");
-
-    if (oldReservation.length) {
-      res.status(200).send(oldReservation);
+      res.status(200).send(oldReservations);
+    } else if (req.method === "DELETE") {
+      await Reservation.findByIdAndDelete(req.query.bookId as string);
+      res.status(200).end();
     } else {
-      res.status(200).send([]);
+      res.status(404).end();
     }
-  } else if (req.method === "DELETE") {
-    await Reservation.findByIdAndDelete({ _id: req.query.bookId });
-
-    res.status(200).end();
-  } else {
-    res.end();
-    return false;
+  } catch (error) {
+    console.error("Error handling the request:", error);
+    res.status(500).end();
   }
 };
+
 export default handler;
